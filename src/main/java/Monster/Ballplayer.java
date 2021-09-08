a5800c754d51b25a822be61da7da37f8384915b2
DAHE_JINYI <nishisb15679697110@qq.com>
2021-09-08 18:22:45 +0800

    QQR

---
+++ src/main/java/Monster/Ballplayer.java
@@ -0,0 +1,10 @@
+package Monster;
+
+public class Ballplayer extends Monster {
+    public Ballplayer(){
+        this.name = "丘丘人";
+        this.HP = 20;
+        this.attack = 5;
+    }
+    public void main(String[] args) {}
+}
