6e3d5317ed54135ee1aa6d67dfff68c5a6131000
Yue-plus <Yue_plus@foxmail.com>
2021-09-10 08:50:08 +0800

    mk BadEvents.java

---
+++ src/main/java/RandomEvents/BadEvents.java
@@ -0,0 +1,18 @@
+package RandomEvents;
+
+public class BadEvents {
+    public void BadEvents() {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        // TODO
+    }
+
+    /**
+     * 返回一个坏事件
+     */
+    public void getBadEvent() {
+        System.out.println("这是个坏事件。");
+    }
+}
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,33 +1,27 @@
+import RandomEvents.BadEvents;
 import java.util.Scanner;
 
 public class Start {
     public static void main(String[] args) {
+
+        // 处理输入的对象
         Scanner in = new Scanner(System.in);
+
         Player player = new Player(300, 1000, 1, 10);
         Knapsack knapsack = new Knapsack();
-        Story story = new Story();
+        BadEvents badEvents = new BadEvents();
+//        Story story = new Story();
         System.out.println("（输入[H]获取帮助）");
         while (true) {
             String input = in.nextLine();
             switch (input) {
-                case "B":
-                    knapsack.getKnapsackList();
-                    break;
-                case "H":
-                    getHelp();
-                    break;
-                case "L":
-                    player.getStatus();
-                    break;
-                case "G":
-                    knapsack.addItem("★   | 随处可见的石头");
-                    break;
-                case "Q":
-                    knapsack.removeItem(0);
-                    break;
-                case "Q1":
-                    System.out.println("发现了一只野生的派蒙\n" + "派蒙咬了你一口\n" + "你被吓死了.\n");
-                    break;
+                case "B" -> knapsack.getKnapsackList();
+                case "H" -> getHelp();
+                case "L" -> player.getStatus();
+                case "G" -> knapsack.addItem("★   | 随处可见的石头");
+                case "Q" -> knapsack.removeItem(0);
+                case "Q1" -> System.out.println("发现了一只野生的派蒙\n" + "派蒙咬了你一口\n" + "你被吓死了.\n");
+                case "W" -> badEvents.getBadEvent();
             }
         }
     }
@@ -35,6 +29,7 @@ public class Start {
     public static void getHelp() {
         System.out.println("""
                 操作方法：
+                [W] - 前进一步。
                 [B] - 查看背包。
                 [L] - 查看自身状态。
                 [G] - 捡起一块石头。
